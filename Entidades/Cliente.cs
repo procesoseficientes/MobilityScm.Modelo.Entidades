dbcf6138c9491e7ca249a2f646845004228c3c84
Elder <elucas-2015337@kinal.edu.gt>
2021-01-21 16:01:22 -0600

    Modificacion Cliente.cs

--- Entidades/Cliente.cs
+++ Entidades/Cliente.cs
@@ -9,10 +9,9 @@ using Telerik.OpenAccess.Data.Common;
 namespace MobilityScm.Modelo.Entidades
 {
     [Serializable]
-    public class Cliente : ICliente
+    public class Cliente
     {
-
-        public int CUSTOMER { get; set; }
+        public int? CUSTOMER { get; set; }
 
         public string CODE_CUSTOMER { get; set; }
 
@@ -36,6 +35,10 @@ namespace MobilityScm.Modelo.Entidades
 
         public float? CREDIT_LIMIT { get; set; }
 
+        public int FROM_ERP { get; set; }
+
+        public string TAX_ID_NUMBER { get; set; }
+
         public string SIGN { get; set; }
 
         public string PHOTO { get; set; }
@@ -43,14 +46,33 @@ namespace MobilityScm.Modelo.Entidades
         public string STATUS { get; set; }
 
         public int? NEW { get; set; }
-
         public string GPS { get; set; }
-
         public string CODE_CUSTOMER_HH { get; set; }
+
+        public string FREQUENCY { get; set; }
+
+        public string TAGS { get; set; }
+
+        public string LATITUDE { get; set; }
+
+        public string LONGITUDE { get; set; }
+
+        public string Tags { get; set; }
+
+        public int NotTags { get; set; }
 
         public string REFERENCE { get; set; }
 
+        public string NAME_ROUTE { get; set; }
+
+        public DateTime FechaIncio { get; set; }
+
+        public DateTime FechaFinal { get; set; }
         public DateTime? POST_DATETIME { get; set; }
+
+        public int ID_FREQUENCY { get; set; }
+
+        public int PRIORITY { get; set; }
 
         public string POS_SALE_NAME { get; set; }
 
@@ -64,26 +86,30 @@ namespace MobilityScm.Modelo.Entidades
 
         public string COMMENTS { get; set; }
 
+        public string STATUS_NEW { get; set; }
+
+        public string LOGIN { get; set; }
+
+        public int EXTRA_DAYS { get; set; }
+
+        public decimal DISCOUNT { get; set; }
+
+        public string CODE_PRICE_LIST { get; set; }
+
         public int? ATTEMPTED_WITH_ERROR { get; set; }
 
         public int? IS_POSTED_ERP { get; set; }
+
+        public string IS_POSTED_ERP_DESCRIPTION { get; set; }
 
         public DateTime? POSTED_ERP { get; set; }
 
         public string POSTED_RESPONSE { get; set; }
         public string SALES_PERSON_CODE { get; set; }
 
-        public string LATITUDE { get; set; }
-        public string LONGITUDE { get; set; }
+        public DateTime DATE_START { get; set; }
 
-        public string FREQUENCY_WEEKS { get; set; }
-        public string MONDAY { get; set; }
-        public string TUESDAY { get; set; }
-        public string WEDNESDAY { get; set; }
-        public string THURSDAY { get; set; }
-        public string FRIDAY { get; set; }
-        public string SATURDAY { get; set; }
-        public string SUNDAY { get; set; }
+        public DateTime DATE_END { get; set; }
 
         public string DEPARTAMENT { get; set; }
 
@@ -92,20 +118,91 @@ namespace MobilityScm.Modelo.Entidades
         public string COLONY { get; set; }
 
         public int? UPDATED_FROM_BO { get; set; }
-
         public string SYNC_ID { get; set; }
 
+        public Frecuencia Frecuencia { get; set; }
+
+        public int CODE_FREQUENCY { get; set; }
+
+        public int SUNDAY { get; set; }
+
+        public int MONDAY { get; set; }
+
+        public int TUESDAY { get; set; }
+
+        public int WEDNESDAY { get; set; }
+
+        public int THURSDAY { get; set; }
+
+        public int FRIDAY { get; set; }
+
+        public int SATURDAY { get; set; }
+
+        public int DAY { get; set; }
+        public int FREQUENCY_WEEKS { get; set; }
+
+        public DateTime LAST_DATE_VISITED { get; set; }
+
+        public int HAS_FREQUENCY { get; set; }
+
+        public DateTime LAST_OPTIMIZATION { get; set; }
+
+        public int TRADE_AGREEMENT_ID { get; set; }
+
+        public string SELLER_NAME { get; set; }
+        public string TYPE_TASK { get; set; }
+
+        public string NAME_CHANNEL{ get; set; }
+
+        public string ASOCIADO { get; set; }
+
+        /// <summary>
+        /// Tiene propuesta de frecuencia
+        /// </summary>
+        public int HAS_PROPOSAL { get; set; }
+
+        /// <summary>
+        /// Es nuevo dentro de un poligono. 
+        /// </summary>
         public int IS_NEW { get; set; }
 
-        public string CODE_CUSTOMER_BO { get; set; }
+        public string CODE_CHANNEL { get; set; }
 
+        public int POLYGON_ID { get; set; }
+
+        public string CODE_CUSTOMER_BO { get; set; }
         public string FIELD_NAME_FOR_TAX_ID { get; set; }
         public int CREATE_NEW_CUSTOMER_AS_LID { get; set; }
         public int? DEFAULT_GROUP_CODE { get; set; }
 
+        public string SCOUTING_ROUTE { get; set; }
+
         public int OWNER_ID { get; set; }
+
+        public string OWNER_NAME { get; set; }
+
+        public string NAME_USER { get; set; }
+
+        public string PHOTO_2 { get; set; }
+
+        public string PHOTO_3 { get; set; }
+
+        public string PHOTO_4 { get; set; }
 
         public string IS_FROM { get; set; }
 
+        public string JSON { get; set; }
+
+        public string DOC_SERIE { get; set; }
+
+        public int DOC_NUM { get; set; }
+
+        public string POSTED_BY { get; set; }
+
+        public int IS_READY_TO_SEND { get; set; }
+
+        public int IS_SENDING { get; set; }
+
+        public DateTime LAST_UPDATE_IS_SENDING { get; set; }
     }
 }
