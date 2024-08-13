802cfca43a64c1625f9e24208f6e59aeeb923268
DenisArango1 <denis.a@procesoseficientes.com>
2024-08-12 19:55:44 -0600

    Add Missing CreditMemo

---
+++ Entidades/CreditMemo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MobilityScm.Modelo.Entidades.Entidades
+{
+    public class CreditMemo
+    {
+        public int ERP_RECEPTION_DOCUMENT_HEADER_ID { get; set; }
+
+        public string TYPE { get; set; }
+
+        public string CODE_SUPPLIER { get; set; }
+
+        public string CODE_CLIENT { get; set; }
+
+        public DateTime ERP_DATE { get; set; }
+
+        public DateTime LAST_UPDATE { get; set; }
+
+        public string LAST_UPDATE_BY { get; set; }
+
+        public int ATTEMPTED_WITH_ERROR { get; set; }
+
+        public int IS_POSTED_ERP { get; set; }
+
+        public DateTime POSTED_ERP { get; set; }
+
+        public string ERP_REFERENCE { get; set; }
+
+        public int IS_AUTHORIZED { get; set; }
+
+        public int IS_COMPLETE { get; set; }
+
+        public int TASK_ID { get; set; }
+
+        public int EXTERNAL_SOURCE_ID { get; set; }
+
+        public string NAME_SUPPLIER { get; set; }
+
+        public string OWNER { get; set; }
+
+        public int IS_FROM_WAREHOUSE_TRANSFER { get; set; }
+    }
+}
